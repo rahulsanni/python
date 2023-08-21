67468e789c4e81a8f83881765fa2ce89b1872148
rahulsanni <77004590+rahulsanni@users.noreply.github.com>
2023-08-21 18:36:42 +0530

    Add files via upload

---
+++ desktop.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### data types
+
+# In[19]:
+
+
+firstname="rahul"
+firstname,type(firstname)
+
+
+# In[20]:
+
+
+age=21
+age,type(age)
+
+
+# In[ ]:
+
+
+
+
